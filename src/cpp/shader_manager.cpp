785b07700493d601757d319c7c9f2f422eacd253
YUTO YOSHIDA <241327@st.yoshida-g.ac.jp>
2026-03-23 16:40:30 +0900

    feat : シェーダーに必要なcpp.hユイカ

---
+++ src/cpp/shader_manager.cpp
@@ -0,0 +1,94 @@
+//===================================================
+//
+// シェーダーの初期化を行うs [shader_manager.cpp]
+// Author:YUTO YOSHIDA
+//
+//===================================================
+
+//***************************************************
+// インクルードファイル
+//***************************************************
+#include "shader_manager.h"
+#include "manager.h"
+#include "renderer.h"
+#include "shader.h"
+
+//***************************************************
+// 定数宣言
+//***************************************************
+namespace ShaderConst
+{
+    constexpr const char* OUTLINE_PATH                  = "data/SHADER/outLine.hlsl";                   // アウトラインシェーダーのパス
+    constexpr const char* GAUS_BLUR_PATH                = "data/SHADER/gausblur.hlsl";                  // ガウスブラーのパス
+    constexpr const char* LUMINANCE_PATH                = "data/SHADER/luminance.hlsl";                 // 輝度抽出のパス
+    constexpr const char* BLOOM_PATH                    = "data/SHADER/bloom.hlsl";                     // ブルームのパス
+    constexpr const char* SHADOW_MAP_PATH               = "data/SHADER/shadow_map.hlsl";                // シャドウマップのパス
+    constexpr const char* SHADOW_MAP_RECIEVE_PATH       = "data/SHADER/shadow_map_reciever.hlsl";       // シャドウマップを受けるオブジェクトのパス
+    constexpr const char* SHADOW_MAP_RECIEVE_MT_PATH    = "data/SHADER/shadow_map_reciever_mt.hlsl";    // シャドウマップを受けるマルチテクスチャオブジェクトのパス
+    constexpr const char* TECHNIQUE                     = "tech";                                       // テクニック宣言
+}
+
+//===================================================
+// コンストラクタ
+//===================================================
+CShaderManager::CShaderManager() : 
+    m_apShader()
+{
+}
+
+//===================================================
+// デストラクタ
+//===================================================
+CShaderManager::~CShaderManager()
+{
+    // シェーダー分回す
+    for (auto& shader : m_apShader)
+    {
+        // メモリの破棄
+        shader.reset();
+    }
+}
+
+//===================================================
+// 生成処理
+//===================================================
+std::unique_ptr<CShaderManager> CShaderManager::Create(void)
+{
+    // インスタンスの生成
+    auto pInstance = std::make_unique<CShaderManager>();
+
+    // 初期化処理
+    if (FAILED(pInstance->Init()))
+    {
+        pInstance.reset();
+        return nullptr;
+    }
+
+    return pInstance;
+}
+
+//===================================================
+// 初期化処理
+//===================================================
+HRESULT CShaderManager::Init(void)
+{
+    // シャドウマップのシェーダーの生成
+    m_apShader[TYPE_SHADOW_MAP] = std::make_unique<CShader>();
+
+    // 初期化処理
+    if (FAILED(m_apShader[TYPE_SHADOW_MAP]->Init(ShaderConst::SHADOW_MAP_PATH, ShaderConst::TECHNIQUE)))
+    {
+        return E_FAIL;
+    }
+
+    // シャドウマップを受けるシェーダーの生成
+    m_apShader[TYPE_SHADOW_MAP_RECIEVE] = std::make_unique<CShader>();
+
+    // 初期化処理
+    if (FAILED(m_apShader[TYPE_SHADOW_MAP_RECIEVE]->Init(ShaderConst::SHADOW_MAP_RECIEVE_PATH, ShaderConst::TECHNIQUE)))
+    {
+        return E_FAIL;
+    }
+
+    return S_OK;
+}
---
+++ src/include/shader.h
@@ -0,0 +1,55 @@
+//===================================================
+//
+// シェーダーのクラスの基底クラス [shader.h]
+// Author:YUTO YOSHIDA
+//
+//===================================================
+
+//***************************************************
+// 多重インクルード防止
+//***************************************************
+#ifndef _SHADER_H_
+#define _SHADER_H_
+
+//***************************************************
+// インクルードファイル
+//***************************************************
+#include "variant"
+
+//***************************************************
+// シェーダーのクラスの定義
+//***************************************************
+class CShader
+{
+public:
+
+	// 型の設定
+	using Parameter = std::variant<float, D3DXVECTOR4, D3DXMATRIX>;
+
+	CShader();
+	~CShader();
+
+	HRESULT Init(const char* pShaderFile, const char* pTech);
+
+	void		Begin					(void);
+	void		BeginPass				(const int nPass = 0);
+	void		End						(void);
+	void		EndPass					(void);
+	void		SetValue				(const char* pKey, const void* pData, size_t size);
+	void		SetFloat				(const char* pKey, const float fParam);
+	void		SetFloatArray			(const D3DXHANDLE& hHandle,const FLOAT* pParam, const UINT count);
+	void		SetVectorFloatArray		(const D3DXHANDLE& hHandle, const D3DXVECTOR4* pVector4, const UINT count);
+	void		SetVector				(const char* pKey, const D3DXVECTOR4& vector4);
+	void		SetMatrix				(const char* pKey, const D3DXMATRIX& matrix);
+	void		SetTexture				(const char* pKey, const int nTextureID);
+	void		SetTexture				(const char* pKey, const LPDIRECT3DTEXTURE9& pTextureID);
+	D3DXHANDLE	GetHandle				(const char* pKey) const;
+	void		SetBool					(const char* pKey, const bool bEnable);
+	void		CommitChange			(void);
+private:
+	//LPDIRECT3DVERTEXDECLARATION9 m_pVertexDecl;		// 頂点バッファの構造をDirectxに伝える変数
+	LPD3DXEFFECT m_pEffect;								// HLSLのエフェクト
+	std::unordered_map<std::string, Parameter> m_Param;	// パラメータ
+	UINT m_nNumPass;									// パスの総数
+};
+#endif
